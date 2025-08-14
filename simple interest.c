cf2eb1bd53b9184987c5a88e9e5a68f5ea86c963
Ridhi443 <ridhigulati156@gmail.com>
2025-08-14 20:18:33 +0530

    Add files via upload

---
+++ simple interest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int p,t;
+    float r,si;
+    printf("enter principal amount");
+    scanf("%d",&p);
+    printf("enter time period");
+    scanf("%d,&t");
+    printf("enter the rate of interest");
+    scanf("%d,&r");
+    si=(p*r*t)/100;
+    printf("simple interest=%f\n,si");
+    return 0;
+}

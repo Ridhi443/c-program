820074d2d9069f888697b24ca6361053cb1f1e4e
Ridhi443 <ridhigulati156@gmail.com>
2025-08-14 09:56:56 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+float pi=3.14;
+int r;
+int area;
+printf("\nenter the radius");
+scanf("%d,&r");
+area=pi*r*r;
+printf("\nenter the area of circle");
+return 0;
+}

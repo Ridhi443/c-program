df8f27b863f1584e0cbf3258683bc8eb13a2935e
Ridhi443 <ridhigulati156@gmail.com>
2025-08-14 09:10:52 +0530

    Add files via upload

---
+++ c to f.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float c,f;
+    printf("\n enter the value of celcius");
+    scanf("%f,&c");
+    f=((9*c)+160)/5;
+    printf("\n enter the value of faranhiet%f\n,f");
+    return 0;
+}

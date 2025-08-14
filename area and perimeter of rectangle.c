2dbb92325d7cbf2b8f91fac33b5c7b230a7a59c8
Ridhi443 <ridhigulati156@gmail.com>
2025-08-14 09:39:37 +0530

    Add files via upload

---
+++ area and perimeter of rectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int perimeter,area;
+    int l,b;
+    printf("\nenter the length");
+    scanf("%d,&l");
+    printf("\nenter the breadth");
+    scanf(%d,&b);
+    perimeter=2*(l+b);
+    area=l*b;
+    printf("\nenter the perimeter of rectangle%d",perimter);
+    printf("\nenter the area of rectangle%d",area);
+    return 0;
+}

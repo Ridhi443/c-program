51d008ce2d4bf133647666cc1417213b77d278eb
Ridhi443 <ridhigulati156@gmail.com>
2025-08-26 08:33:13 +0530

    swapping numbers.c

--- swapping numbers.c
+++ swapping numbers.c
@@ -3,9 +3,10 @@ int main()
 {
     int a=10,b=11;
   printf("Before swapping: a = %d, b = %d\n", a, b);
-   a=10; 
+   a = 10; 
     a = b;   
     b = 11;
  printf("After swapping: a = %d, b = %d\n", a, b);
  return 0;
 }
+

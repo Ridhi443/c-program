4b1a7176b7b509e8537695e891a9edb48e82a6ed
Ridhi443 <ridhigulati156@gmail.com>
2025-08-26 08:38:32 +0530

    area of circle.c

--- area of circle.c
+++ area of circle.c
@@ -2,10 +2,9 @@
 int main()
 {
 float pi=3.14;
-int r;
-int area;
+int r,area;
 printf("\nenter the radius");
-scanf("%d,&r");
+scanf("%d",&r);
 area=pi*r*r;
 printf("\nenter the area of circle");
 return 0;

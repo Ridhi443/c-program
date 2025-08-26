fc590555b75527100e2817db40131441462936d7
Ridhi443 <ridhigulati156@gmail.com>
2025-08-26 08:35:28 +0530

    simple interest.c

--- simple interest.c
+++ simple interest.c
@@ -6,10 +6,11 @@ int main ()
     printf("enter principal amount");
     scanf("%d",&p);
     printf("enter time period");
-    scanf("%d,&t");
+    scanf("%d",&t);
     printf("enter the rate of interest");
-    scanf("%d,&r");
+    scanf("%d",&r);
     si=(p*r*t)/100;
-    printf("simple interest=%f\n,si");
+    printf("simple interest=%f\n",si);
     return 0;
+
 }

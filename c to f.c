a3e278c31581d0dc7a4ca9445d4596268c0ce751
Ridhi443 <ridhigulati156@gmail.com>
2025-08-26 08:36:33 +0530

    Update c to f.c

--- c to f.c
+++ c to f.c
@@ -3,8 +3,8 @@ int main()
 {
     float c,f;
     printf("\n enter the value of celcius");
-    scanf("%f,&c");
+    scanf("%f",&c);
     f=((9*c)+160)/5;
-    printf("\n enter the value of faranhiet%f\n,f");
+    printf("\n enter the value of faranhiet%f\n",f);
     return 0;
 }
